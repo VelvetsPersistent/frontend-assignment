1a4aa23bc4fadc1274a97f8011036514b8785307
Smita Parajuli <108572943+VelvetsPersistent@users.noreply.github.com>
2023-05-30 21:49:31 +0545

    Update page.tsx
    basic edits

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
 export default function Home() {
-  return <p>Good luck !</p>;
+  return <p>Hello there !</p>;
+  return <p>I am still newbie at this.</p>;
+  return <p>I did my best upto hosting this site.</p>;
+  return <p>Thank you for the consideration</p>;
 }
